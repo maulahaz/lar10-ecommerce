5e70161c50f6673ffc9b42b31cfed48355ebb15e
maulahaz <maulahaz@gmail.com>
2024-05-07 08:46:43 +0400

    benerin TailwindCSS, ada error di postCss.config.js

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -1,4 +1,4 @@
 <div>
-    <h1>Welcome</h1>
+    <h1 class="text-4xl text-green-500">Welcome</h1>
     {{-- Close your eyes. Count to one. That is how long forever feels. --}}
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-// module.exports = {
-export default {
+module.exports = {
+// export default {
   content: [
+    "./index.html', './src/**/*.{js,ts,jsx,tsx}",
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
